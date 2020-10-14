46ec6300df62d78f334ef232fc89ccfd1dbc52fe
HolzerSoahita <holzersoahita@gmail.com>
2020-10-14 15:21:07 +0300

    Create pdf.mp3

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pyttsx3
 import PyPDF2
 
-book = open("oop.pdf", 'rb')
-pdfReader=PyPDF2.PdfFileReader(book)
+book = open("file.pdf", 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
@@ -17,20 +17,19 @@ for voice in voices:
     print(" - Gender: %s" % voice.gender)
     print(" - Age: %s" % voice.age)
 
-# Voice IDs pulled from engine.getProperty('voices')
-# These will be system specific
+# Voice IDs from the system
 en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 fr_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_FR-FR_HORTENSE_11.0"
 
 # Use female English voice
 speaker.setProperty('voice', en_voice_id)
 
-#Read all pdf
-for num in range(7,pages):
-    page = pdfReader.getPage(7)
-    text = page.extractText()
-    speaker.say(text)
-    speaker.runAndWait()
+text = ""
+# Read all pdf
+for num in range(1, pages):
+    page = pdfReader.getPage(1)
+    text += page.extractText()
+    # speaker.say(text)
 
-# Uncomment this if no runAndWait above
-# speaker.runAndWait()
+speaker.save_to_file(text, 'file.mp3')
+speaker.runAndWait()
